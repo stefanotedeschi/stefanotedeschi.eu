96863bd749ff8bb27a3ee81c3c7f5212b26f3fe0
Stefano Tedeschi <stefano.tedeschi@unito.it>
2023-09-15 10:29:15 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+jQuery( document ).ready(function( $ ) {
+"use strict"
+/*-----------------------------------------------------------------------------------*/
+/*    PORTFOLIO FILTER
+/*-----------------------------------------------------------------------------------*/
+
+$('#Container').cubePortfolio({
+    defaultFilter: '.year',
+    animationType: 'skew'
+});
+
+//$('#Container').mixItUp();
+
+});
+
+
+
+
+
